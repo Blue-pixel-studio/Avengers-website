754dce5472930eca2aea7806106bb4122b3a8603
veluvj <veluvijay1804@gmail.com>
2020-10-31 16:58:55 +0530

    funal submission

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,19 +7,37 @@ import { ViewportScroller } from '@angular/common';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Avengers Fitness Studio';
 
-
-
+  isActive: number = 0;
 
   constructor(private viewportScroller: ViewportScroller) {}
 
 
 
-  public onClick(elementId: string):void {
+  public onClick(elementId: string, value:number):void {
     
     // this.viewportScroller.scrollToAnchor(elementId);
 
+    console.log(value)
+    switch(value){
+
+      case 0:
+        this.isActive = 0;
+        break;
+
+      case 1:
+        this.isActive = 1;
+        break;
+       
+      case 2:
+        this.isActive = 2;
+        break;
+        
+      default:
+        this.isActive =0;
+        break;
+    }
     let el = document.getElementById(elementId);
     el.scrollIntoView({ behavior: "smooth"})
 
